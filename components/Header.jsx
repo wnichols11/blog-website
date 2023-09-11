0be0de95750bfafdf2ac4030935dd53acea2ac50
wnichols11 <nichols.walker11@gmail.com>
2023-09-11 12:19:20 -0400

    complete version 1

--- components/Header.jsx
+++ components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Melissa NewComb
+              Melissa Newcomb
             </span>
           </Link>
         </div>
